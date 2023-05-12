2c416209b11f1d5fea1f4333287a9363bba02ec9
Vaibhav <vaibhavagarwal693@gmail.com>
2023-05-12 18:39:51 +0530

    no

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './App.css';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <LandingPage />
   );
 }
 
